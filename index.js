b9aa885d0f7af020643f2fd3770d68882f85a4ce
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-05-19 00:36:56 +0000

    Should be able to fetch subtitles

--- index.js
+++ index.js
@@ -5,11 +5,36 @@ import path from 'path'
 const qRx = /Auto\-detected query: \[(.+)\]/;
 const skippedRx = /.+\] because \[(.+)\]/;
 const renamedRx = /.+\] to \[(.+)\]/;
+const subRx = /Writing \[.+\] to \[(.+)\]/;
 
 /**
  * FileBot node wrapper.
  */
 class FileBot {
+  /**
+   * Rename media files
+   * @param config
+   * @param cb
+   * @returns {Promise} if no callback is provided.
+   */
+  static getSubtitles(config, cb) {
+    let command = `filebot -get-missing-subtitles ${path.normalize(config.path)}`;
+    let res = {message: undefined, fetched: []};
+
+    return new Promise(resolve => {
+      let child = exec(command);
+
+      child.stdout.on('data', chunk => {
+        if (!res.message)
+          res.message = chunk.split('\n')[0];
+        else if (subRx.test(chunk))
+          res.fetched.push(path.basename(subRx.exec(chunk)[1]));
+      });
+
+      child.stdout.on('close', () => cb ? cb(res) : resolve(res));
+    });
+  }
+
   /**
    * Rename media files
    * @param config
@@ -23,13 +48,13 @@ class FileBot {
     return new Promise(resolve => {
       let child = exec(command);
 
-      child.stdout.on('data', chunck => {
-        if (!res.query && qRx.test(chunck))
-          res.query = qRx.exec(chunck)[1];
-        else if (skippedRx.test(chunck))
-          res.skipped.push(path.basename(skippedRx.exec(chunck)[1]));
-        else if (renamedRx.test(chunck))
-          res.renamed.push(path.basename(renamedRx.exec(chunck)[1]));
+      child.stdout.on('data', chunk => {
+        if (!res.query && qRx.test(chunk))
+          res.query = qRx.exec(chunk)[1];
+        else if (skippedRx.test(chunk))
+          res.skipped.push(path.basename(skippedRx.exec(chunk)[1]));
+        else if (renamedRx.test(chunk))
+          res.renamed.push(path.basename(renamedRx.exec(chunk)[1]));
       });
 
       child.stdout.on('close', () => cb ? cb(res) : resolve(res));
